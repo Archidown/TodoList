7d6f297751ef4c4ffb48e849860c80239f5365f9
Djakhar <djakhar.khadjiev@etu.unistra.fr>
2026-09-06 19:16:46 +0200

    feat: new button for datePicker and right arrangement

--- shared/src/commonMain/kotlin/com/example/todo_list/view/DatePickerItem.kt
+++ shared/src/commonMain/kotlin/com/example/todo_list/view/DatePickerItem.kt
@@ -2,6 +2,8 @@ package com.example.todo_list.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -15,9 +17,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.todo_list.view.themes.Accent
-import com.example.todo_list.view.themes.OnAccent
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
 import kotlin.time.Instant
@@ -25,11 +28,50 @@ import kotlin.time.Instant
 @OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun DatePickerItem(onDatePicked: (String) -> Unit, onDismiss: () -> Unit) {
+    val state = rememberDatePickerState(initialDisplayMode = DisplayMode.Input)
+    val date = state.selectedDateMillis
     Column(
         verticalArrangement = Arrangement.spacedBy(8.dp),
         modifier = Modifier.padding(20.dp)
     ) {
-        val state = rememberDatePickerState(initialDisplayMode = DisplayMode.Input)
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                onClick = onDismiss,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Accent
+                )
+            ){
+                Text(
+                    text = "Cancel",
+                    fontSize = 20.sp
+                )
+            }
+            Button(
+                onClick = {
+                    if (date != null) {
+                        val correctDate = Instant.fromEpochMilliseconds(date)
+                            .toLocalDateTime(TimeZone.UTC)
+                            .date
+                        onDatePicked(correctDate.toString())
+                        onDismiss()
+
+                    }
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Accent
+                )
+            ) {
+                Text(
+                    text = "OK",
+                    fontSize = 20.sp
+                )
+            }
+        }
         DatePicker(
             state = state,
             modifier = Modifier.padding(16.dp),
@@ -47,24 +89,5 @@ fun DatePickerItem(onDatePicked: (String) -> Unit, onDismiss: () -> Unit) {
                 selectedYearContainerColor = Accent
             )
         )
-        val date = state.selectedDateMillis
-        Button(
-            onClick = {
-                if (date != null) {
-                    val correctDate = Instant.fromEpochMilliseconds(date)
-                        .toLocalDateTime(TimeZone.UTC)
-                        .date
-                    onDatePicked(correctDate.toString())
-                    onDismiss()
-
-                }
-            },
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Accent,
-                contentColor = OnAccent
-            )
-        ) {
-            Text("Ok")
-        }
     }
 }
