15e2af4cd3ea4fba70a4955593abf67df37e6a29
Djakhar Khadjiev <djakharkhadjiev@gmail.com>
2026-09-07 17:04:06 +0200

    feat: adding cancel and save button in TaskEdit

--- shared/src/commonMain/kotlin/com/example/todo_list/view/ModalBottomSheetTaskEdit.kt
+++ shared/src/commonMain/kotlin/com/example/todo_list/view/ModalBottomSheetTaskEdit.kt
@@ -1,11 +1,15 @@
 package com.example.todo_list.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
@@ -14,9 +18,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.todo_list.view.themes.Accent
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -30,9 +36,32 @@ fun ModalBottomSheetTaskEdit(
         onDismissRequest = { onDismissRequest() },
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 15.dp),
+            .padding(top = 5.dp),
         containerColor = MaterialTheme.colorScheme.background
     ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Button(
+                onClick = onDismissRequest,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Accent
+                )
+            ) {
+                Text(text = "Cancel", fontSize = 20.sp)
+            }
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Transparent,
+                    contentColor = Accent
+                )
+            ) {
+                Text(text = "Save", fontSize = 20.sp)
+            }
+        }
         Column(
             modifier = Modifier
                 .padding(15.dp)
@@ -47,8 +76,11 @@ fun ModalBottomSheetTaskEdit(
                 fontSize = 25.sp,
                 color = MaterialTheme.colorScheme.onPrimary
             )
-            HorizontalDivider(color = MaterialTheme.colorScheme.outlineVariant, modifier = Modifier.padding(vertical = 8.dp))
-            if (description!=""){
+            HorizontalDivider(
+                color = MaterialTheme.colorScheme.outlineVariant,
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
+            if (description != "") {
                 Text(description)
                 HorizontalDivider(
                     color = MaterialTheme.colorScheme.outlineVariant,
