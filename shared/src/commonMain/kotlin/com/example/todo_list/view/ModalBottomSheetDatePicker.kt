88c3ceff4d91a9b46257bbbf0bc1782892f45430
Djakhar Khadjiev <djakharkhadjiev@gmail.com>
2026-09-15 13:54:04 +0200

    fix: DatePickerSheet only shows 3week

--- shared/src/commonMain/kotlin/com/example/todo_list/view/ModalBottomSheetDatePicker.kt
+++ shared/src/commonMain/kotlin/com/example/todo_list/view/ModalBottomSheetDatePicker.kt
@@ -3,12 +3,14 @@ package com.example.todo_list.view
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ModalBottomSheetDatePicker(onDismiss: () -> Unit, onDatePicked: (String) -> Unit) {
     ModalBottomSheet(
+        sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
         onDismissRequest = onDismiss,
         dragHandle = null,
         containerColor = MaterialTheme.colorScheme.background
