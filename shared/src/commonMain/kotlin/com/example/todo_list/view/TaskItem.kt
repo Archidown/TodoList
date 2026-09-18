976cdf77d58b214eb096db4f658f97d2c9b505fc
Djakhar Khadjiev <djakharkhadjiev@gmail.com>
2026-09-18 11:03:13 +0200

    feat: better UI for taskItem

--- shared/src/commonMain/kotlin/com/example/todo_list/view/TaskItem.kt
+++ shared/src/commonMain/kotlin/com/example/todo_list/view/TaskItem.kt
@@ -7,8 +7,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,9 +32,12 @@ fun TaskItem(
             .background(MaterialTheme.colorScheme.background)
             .fillMaxWidth()
             .clickable(onClick = onClick)
-            .padding(vertical = 12.dp)
+            .padding(12.dp)
     ) {
         Checkbox(
+            modifier = Modifier
+                .size(20.dp)
+                .padding(8.dp),
             checked = task.isDone,
             onCheckedChange = {
                 onFinished(task)
@@ -43,7 +48,8 @@ fun TaskItem(
             ),
         )
         Column(
-            verticalArrangement = Arrangement.SpaceEvenly
+            verticalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.padding(start = 12.dp)
         ) {
             Text(
                 text = task.title,
@@ -51,15 +57,20 @@ fun TaskItem(
                 fontWeight = FontWeight.Bold,
                 color = MaterialTheme.colorScheme.onPrimary
             )
-            Text(
-                text = task.description,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                color = MaterialTheme.colorScheme.onPrimary
-            )
+            if (task.description != "")
+                Text(
+                    text = task.description,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    color = MaterialTheme.colorScheme.onPrimary
+                )
             Text(
                 text = task.date,
                 color = MaterialTheme.colorScheme.onPrimary
+            )
+            HorizontalDivider(
+                color = MaterialTheme.colorScheme.outlineVariant,
+                modifier = Modifier.padding(vertical = 8.dp)
             )
         }
 
